e4db4a569e096b0d32c343dc0f11c36daed3ab28
SohamG <sohamg2@gmail.com>
2019-04-07 14:47:18 +0530

    Started index.js, added args extractor, working on getting hours/minutes now

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-#starting
+"use strict";
+const Discord = require('discord.js');
+const timerlib = require('easytimer.js').Timer;
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+let timer = new timerlib();
+
+let getArgs = (msg, cmd) => {
+    let command = prefix + msg.content;
+    let args = command.slice(command.indexOf(cmd) + cmd.length);
+    return args;
+};
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const prefix = "uwu ";
+client.on('message', message => {
+    if (!message.content.startsWith(prefix)) return;
+ 
+    if (message.content.startsWith(prefix + "start ")) {
+        let commandRecd = "start ";
+        let args = getArgs(message, commandRecd);
+        message.reply(`Args are \`\`\`${args}\`\`\` `);
+    }
+});
+
+client.login(auth.token);
